e4277f0955bfa5c54b8ed2ff2cd16b37515f1dd5
steve-pires <steve.martins.pires@gmail.com>
2016-03-28 17:51:50 +0200

    Refactoring code.
    Created methods for each loops, moved methods to follow main order.

--- Skynet/src/Player.java
+++ Skynet/src/Player.java
@@ -11,32 +11,61 @@ class Player {
 	
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
+        // nodes value is not needed in this implementation, but it needs to be consumed
         int nodes = in.nextInt(); // the total number of nodes in the level, including the gateways
         int links = in.nextInt(); // the number of links
         int exits = in.nextInt(); // the number of exit gateways
         
-        linksMap = new HashMap<>();
-        for (int i = 0; i < links; i++) {
-            int linkStartNode = in.nextInt(); // linkStartNode and linkEndNode defines a link between these nodes
-            int linkEndNode = in.nextInt();
-            Player.addNodeWithNewLink(linkStartNode, linkEndNode);
-        }
+        Player.collectLinks(links, in);
  
-        Player.extractExitsList(exits, in);
+        Player.collectExitsList(exits, in);
 
         // game loop
         while (true) {
             int agentPosition = in.nextInt(); // The index of the node on which the Skynet agent is positioned this turn
             int linkToSeverEndNode = findLinkToSevereEndNode(agentPosition);
 
-			System.out.println(agentPosition + " " + linkToSeverEndNode);
-
             // Write an action using System.out.println()
             // To debug: System.err.println("Debug messages...");
             
-			//   System.out.println("0 1"); // Example: 0 1 are the indices of the nodes you wish to sever the link between
+			System.out.println(agentPosition + " " + linkToSeverEndNode);
         }
     }
+
+	private static void collectLinks(int links, Scanner in) {
+        linksMap = new HashMap<>(links);
+        
+		for (int i = 0; i < links; i++) {
+            int linkStartNode = in.nextInt(); // linkStartNode and linkEndNode defines a link between these nodes
+            int linkEndNode = in.nextInt();
+            Player.addNodeWithNewLink(linkStartNode, linkEndNode);
+        }
+	}
+
+    private static void collectExitsList(int exits, Scanner in) {
+        exitsList = new ArrayList<>(exits);
+        
+        for (int i = 0; i < exits; i++) {
+            exitsList.add(in.nextInt()); // adding index of a gateway node to list
+        }
+    }
+
+	/**
+	 * Stores each nodes and its links in a Map.
+	 * Each link is composed of two nodes which are passed in parameters.
+	 * @param linkStartNode
+	 * @param linkEndNode
+	 */
+	private static void addNodeWithNewLink(int linkStartNode, int linkEndNode) {
+	    if(linksMap.containsKey(linkStartNode)) {
+	    	List<Integer> linkedNodes = linksMap.get(linkStartNode);
+	    	linkedNodes.add(linkEndNode);
+	    } else {
+	    	List<Integer> linkedNodes = new ArrayList<>();
+	    	linkedNodes.add(linkEndNode);
+	    	linksMap.put(linkStartNode, linkedNodes);
+	    }
+	}
 
 	private static int findLinkToSevereEndNode(int agentPosition) {
 		int linkToSevereEndNode = -1;
@@ -60,27 +89,4 @@ class Player {
 		return linkToSevereEndNode;
 	}
 
-    private static void extractExitsList(int exits, Scanner in) {
-        exitsList = new ArrayList<>(exits);
-        for (int i = 0; i < exits; i++) {
-            exitsList.add(in.nextInt()); // adding index of a gateway node to list
-        }
-    }
-
-	/**
-	 * Stores each nodes and its links in a Map.
-	 * Each link is composed of two nodes which are passed in parameters.
-	 * @param linkStartNode
-	 * @param linkEndNode
-	 */
-	private static void addNodeWithNewLink(int linkStartNode, int linkEndNode) {
-	    if(linksMap.containsKey(linkStartNode)) {
-	    	List<Integer> linkedNodes = linksMap.get(linkStartNode);
-	    	linkedNodes.add(linkEndNode);
-	    } else {
-	    	List<Integer> linkedNodes = new ArrayList<>();
-	    	linkedNodes.add(linkEndNode);
-	    	linksMap.put(linkStartNode, linkedNodes);
-	    }
-	}
 }
