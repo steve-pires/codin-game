22d3212c100b0968310d8edc0db390a8f924a7cb
steve-pires <steve.martins.pires@gmail.com>
2016-03-28 13:29:39 +0200

    Refactoring.
    Clearer variable names.

--- Skynet/src/Player.java
+++ Skynet/src/Player.java
@@ -10,20 +10,20 @@ class Player {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt(); // the total number of nodes in the level, including the gateways
-        int L = in.nextInt(); // the number of links
-        int E = in.nextInt(); // the number of exit gateways
-        for (int i = 0; i < L; i++) {
-            int N1 = in.nextInt(); // N1 and N2 defines a link between these nodes
-            int N2 = in.nextInt();
+        int nodes = in.nextInt(); // the total number of nodes in the level, including the gateways
+        int links = in.nextInt(); // the number of links
+        int exits = in.nextInt(); // the number of exit gateways
+        for (int i = 0; i < links; i++) {
+            int currentNode1 = in.nextInt(); // N1 and N2 defines a link between these nodes
+            int currentNode2 = in.nextInt();
         }
-        for (int i = 0; i < E; i++) {
-            int EI = in.nextInt(); // the index of a gateway node
+        for (int i = 0; i < exits; i++) {
+            int currentExit = in.nextInt(); // the index of a gateway node
         }
 
         // game loop
         while (true) {
-            int SI = in.nextInt(); // The index of the node on which the Skynet agent is positioned this turn
+            int agentPosition = in.nextInt(); // The index of the node on which the Skynet agent is positioned this turn
 
             // Write an action using System.out.println()
             // To debug: System.err.println("Debug messages...");
